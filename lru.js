1e2eb0d7a072d6d210295bb451f59e81a2a2a1f9
mateenshamsi <matinshamsi16@gmail.com>
2025-03-10 15:09:47 +0530

    JS INTERVIEW

---
+++ lru.js
@@ -0,0 +1,17 @@
+class LRUCache{
+    constructor (capacity){
+        this.capacity = Number(capacity) 
+        this.map =  new Map() 
+        this.head = null 
+        this.tail = null 
+    }
+    get(key){
+
+    }
+    put(key,value){ 
+       if(this.map.has(key))
+        { 
+            
+        }         
+    }
+}
